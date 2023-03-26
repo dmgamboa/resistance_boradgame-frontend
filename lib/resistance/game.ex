0e53b0c3eb2e138d0a3202ea7fdf637fc8338825
KeYang <kyang72@my.bcit.ca>
2023-03-25 17:05:23 -0700

    add public API to Game Server

--- lib/resistance/game.ex
+++ lib/resistance/game.ex
@@ -4,9 +4,53 @@ defmodule Game.Server do
   Store game state
   """
 
+  #public APIs
+  defmodule Player do
+    defstruct [
+      :name,
+      :role,
+      :is_king
+    ]
+  end
+
   def start_link(player_names) do
     GenServer.start_link(__MODULE__, player_names, name: __MODULE__)
   end
+
+  def remove_player(game, player_name) do
+    GenServer.cast(__MODULE__, {:remove_player, player_name})
+
+  end
+
+  def get_player_info(game, player_name) do
+    GenServer.call(__MODULE__, {:get_player_info, player_name})
+  end
+
+  def get_king(game) do
+    GenServer.call(__MODULE__, :get_king)
+  end
+
+  def add_quest_member(game, player_name) do
+    GenServer.cast(__MODULE__, {:add_quest_member, player_name})
+  end
+
+  def remove_quest_member(game, player_name) do
+    GenServer.cast(__MODULE__, {:remove_quest_member, player_name})
+  end
+
+  def confirm_team(game) do
+    GenServer.cast(__MODULE__, :confirm_team)
+  end
+
+  def vote_for_team(game, player_name, vote) do
+    GenServer.cast(__MODULE__, {:vote_for_team, player_name, vote})
+  end
+
+  def vote_for_mission(game, player_name, vote) do
+    GenServer.cast(__MODULE__, {:vote_for_mission, player_name, vote})
+  end
+
+
 
   @impl true
   def init(player_names) do
@@ -20,6 +64,17 @@ defmodule Game.Server do
       current_mission_votes: [],  #[:success | :fail]
     }
     {:ok, state}
+  end
+
+
+
+  ### subscribe and broadcast functions
+  def subscribe() do
+    Phoenix.PubSub.subscribe(Resistance.PubSub, "game")
+  end
+
+  defp broadcast(event, payload) do
+    Phoenix.PubSub.broadcast(Resistance.PubSub, "game", {event, payload})
   end
 
 
