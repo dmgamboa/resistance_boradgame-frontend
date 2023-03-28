380b2730ce3e85b4ada1ff818788ab6fb87ce6a6
KeYang <kyang72@my.bcit.ca>
2023-03-27 22:30:08 -0700

    implement getters

--- lib/resistance/game.ex
+++ lib/resistance/game.ex
@@ -17,19 +17,16 @@ defmodule Game.Server do
     end
   end
 
-  @impl true
-  def handle_call(:get_players, _from, state) do
-    {:reply, state.players, state}
-  end
 
   def start_link(player_names) do
     GenServer.start_link(__MODULE__, player_names, name: __MODULE__)
   end
 
-  def remove_player(player_name) do
-    GenServer.cast(__MODULE__, {:remove_player, player_name})
+  # don't account for player leaving for now
+  # def remove_player(player_name) do
+  #   GenServer.cast(__MODULE__, {:remove_player, player_name})
 
-  end
+  # end
 
   def get_player_info(player_name) do
     GenServer.call(__MODULE__, {:get_player_info, player_name})
@@ -63,6 +60,9 @@ defmodule Game.Server do
     GenServer.cast(__MODULE__, {:vote_for_mission, player_name, vote})
   end
 
+
+  # return a list of players, with 1/3 of them being bad and the rest being good
+  # assign the first player in the list to be the king
   def ramdomly_assign_roles(player_names) do
     num_bad = Enum.count(player_names) / 3 |> Float.ceil |> round
 
@@ -81,14 +81,34 @@ defmodule Game.Server do
   def init(player_names) do
     # feel free to amend the state
     state = %{
-      players: ramdomly_assign_roles(player_names),   #[string, :good | :bad] #TODO: randomly assign good/bad
+      players: ramdomly_assign_roles(player_names),   # a list of Player, order never change during the game
       missions: [],       #[:success | :fail]   #current_mission = length(missions) + 1
-      current_king: nil,    #string
-      current_team: nil,      #[string]
+      current_king:  nil, # a string, player's name
+      current_team: nil,      #[player]
       current_vote: nil,      #[{string (player), :approve | :reject}]
       current_mission_votes: [],  #[:success | :fail]
     }
     {:ok, state}
+  end
+
+
+  # return a player object that is the king
+  @impl true
+  def handle_call(:get_king, _from, state) do
+    {:reply, state.current_king, state}
+  end
+
+  # return a list of players
+  @impl true
+  def handle_call(:get_players, _from, state) do
+    {:reply, state.players, state}
+  end
+
+  # get one particular player by his/her name
+  @impl true
+  def handle_call({:get_player_info, player_name}, _from, state) do
+    player = Enum.find(state.players, fn player -> player.name == player_name end)
+    {:reply, player, state}
   end
 
 
