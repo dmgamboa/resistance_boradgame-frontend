206b1e0f4090b2147e3337479c1d1bd6169b08fe
KeYang <kyang72@my.bcit.ca>
2023-03-29 00:30:17 -0700

    add docs

--- lib/resistance/game.ex
+++ lib/resistance/game.ex
@@ -12,59 +12,81 @@ end
 
 defmodule Game.Server do
   use GenServer
-  @doc """
-  Store game state
-  """
 
-  #public APIs
+
+  @doc """
+  Store game state. The state is a map with the following keys:
+      players: # a list of Player, order never change during the game
+      mission_results: [],       #[:success | :fail]
+      current_king: first_king, # a string, player's name
+      current_team: [],      #[string (player name)]
+      current_vote: [],      #[{string (player name), :approve | :reject}]
+      current_mission_votes: [],  #[:success | :fail]
+      team_rejection_count: int
+  """
 
   def start_link(player_names) do
     GenServer.start_link(__MODULE__, player_names, name: __MODULE__)
   end
 
-  # don't account for player leaving for now
-  # def remove_player(player_name) do
-  #   GenServer.cast(__MODULE__, {:remove_player, player_name})
 
-  # end
-
+  @doc """
+    get one particular player struct by his/her name
+  """
   def get_player_info(player_name) do
     GenServer.call(__MODULE__, {:get_player_info, player_name})
   end
 
+  @doc """
+    return a list of players
+  """
   def get_players() do
     GenServer.call(__MODULE__, :get_players)
   end
 
+  @doc """
+    return the name of the king
+  """
   def get_king() do
     GenServer.call(__MODULE__, :get_king)
   end
 
+@doc """
+  add a player's name to the current mission team and broadcast this player's name
+  """
   def add_quest_member(player_name) do
     GenServer.cast(__MODULE__, {:add_quest_member, player_name})
   end
-
+@doc """
+  remove a player's name from the current mission team and broadcast this player's name
+  """
   def remove_quest_member(player_name) do
     GenServer.cast(__MODULE__, {:remove_quest_member, player_name})
   end
 
+  @doc """
+    broadcast the current team
+  """
   def confirm_team() do
     GenServer.cast(__MODULE__, :confirm_team)
   end
 
+  @doc """
+    add a player's vote to the current vote list. If everyone has voted, broadcast the vote result
+  """
   def vote_for_team(player_name, vote) do
     GenServer.cast(__MODULE__, {:vote_for_team, player_name, vote})
   end
+
 
   def vote_for_mission(vote) do
     GenServer.cast(__MODULE__, {:vote_for_mission, vote})
   end
 
 
-  @doc """
-  return a list of players, with 1/3 of them being bad and the rest being good
-  assign the first_king to be the king
-  """
+  #return a list of players, with 1/3 of them being bad and the rest being good
+  #assign the first_king to be the king
+
   defp make_players(player_names, first_king) do
     num_bad = Enum.count(player_names) / 3 |> Float.ceil |> round
     num_good = length(player_names) - num_bad
@@ -94,28 +116,26 @@ defmodule Game.Server do
     {:ok, state}
   end
 
-  # get one particular player by his/her name
+
   @impl true
   def handle_call({:get_player_info, player_name}, _from, state) do
     player = Enum.find(state.players, fn player -> player.name == player_name end)
     {:reply, player, state}
   end
 
-  # return a list of players
+
   @impl true
   def handle_call(:get_players, _from, state) do
     {:reply, state.players, state}
   end
 
-  # return the name of the king
+
   @impl true
   def handle_call(:get_king, _from, state) do
     {:reply, state.current_king, state}
   end
 
-  @doc """
-  add a player to the current mission team and broadcast
-  """
+
   @impl true
   def handle_cast({:add_quest_member, player_name}, state) do
     new_team = [player_name | state.current_team]
@@ -123,9 +143,7 @@ defmodule Game.Server do
     {:noreply, %{state | current_team: new_team}}
   end
 
-  @doc """
-  remove a player from the current mission team and broadcast
-  """
+
   @impl true
   def handle_cast({:remove_quest_member, player_name}, state) do
     new_team = Enum.filter(state.current_team, fn name -> name != player_name end)
@@ -176,12 +194,12 @@ defmodule Game.Server do
     end
   end
 
-  @doc """
-  start next round, return updated state
-  TODO: should probably wait for all clients to confirm before starting next round,
-  this means adding a new key in state to keep track of which clients have confirmed
-  NOTE: if team is rejected, mission_result is nil
-  """
+
+  # start next round, return updated state
+  # TODO: should probably wait for all clients to confirm before starting next round,
+  # this means adding a new key in state to keep track of which clients have confirmed
+  # NOTE: if team is rejected, mission_result is nil
+
   defp start_next_round(state, mission_result) do
     updated_players = assign_next_king(state.current_king, state.players)
     next_king = find_king(updated_players).name
