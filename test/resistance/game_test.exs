7aadd1aba6f037557886a63ed895879739aad208
KeYang <kyang72@my.bcit.ca>
2023-04-07 17:41:03 -0700

    adding test file under test folder

---
+++ test/resistance/game_test.exs
@@ -0,0 +1,14 @@
+defmodule Resistance.GameTest do
+  use ExUnit.Case, async: true
+  use Phoenix.ChannelTest
+
+  alias Resistance.Game.Server
+
+  test "create new player" do
+    player = Player.new(1, "Alice", :good)
+    assert player.id == 1
+    assert player.name == "Alice"
+    assert player.role == :good
+    assert player.is_king == false
+    assert player.on_quest == false
+  end
