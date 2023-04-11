ff3474df51953cef254357953ee3823cd6caa787
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2023-04-10 21:38:02 -0700

    Add rendering logic for sidebar

--- lib/resistance_web/components/game/side_bar.ex
+++ lib/resistance_web/components/game/side_bar.ex
@@ -1,15 +1,126 @@
 defmodule ResistanceWeb.Game.SideBar do
   use Phoenix.Component
+  import ResistanceWeb.CoreComponents
 
   @doc """
   Creates a side bar for use in the Game LiveView
   """
 
+  attr :quest_outcomes, :any, default: []
+  attr :players, :any, default: []
+  attr :self, Player, required: true
+  attr :stage, :any, required: true
+  attr :team_votes, :any, default: %{}
+
   def side_bar(assigns) do
     ~H"""
         <div class="avalon-side-bar">
-          Side Bar
+          <.quest_outcomes quest_outcomes={@quest_outcomes}/>
+          <.role role={@self.role} />
+          <.player_list
+            players={@players}
+            self={@self}
+            team_votes={@team_votes}
+            stage={@stage}
+          />
         </div>
+    """
+  end
+
+  @doc """
+  Creates the quest outcomes component
+  """
+
+  attr :quest_outcomes, :any, default: []
+
+  def quest_outcomes(assigns) do
+    ~H"""
+      <div class="quest-outcomes">
+        <%=Enum.map(@quest_outcomes, fn q -> %>
+          <div class="outcome">
+            <%= if q == :success do %>
+              <span>✅</span>
+            <% end %>
+            <%= if q == :fail do %>
+              <span>❌</span>
+            <% end %>
+          </div>
+        <% end) %>
+      </div>
+    """
+  end
+
+  @doc """
+  Creates the role section
+  """
+
+  attr :role, :any, required: true
+
+  def role(assigns) do
+    ~H"""
+      <div class="role">
+        You are
+        <%= if @role == :good do %>
+          Arthur's knight
+        <% end %>
+
+        <%= if @role == :bad do %>
+          Mordred's minion
+        <% end %>
+      </div>
+    """
+  end
+
+  @doc """
+  Creates an interactable list of players in the game
+  """
+
+  attr :players, :any, default: []
+  attr :self, Player, required: true
+  attr :stage, :any, required: true
+  attr :team_votes, :any, required: true
+
+  def player_list(assigns) do
+    ~H"""
+      <div class="player-list">
+        <%= Enum.map(@players, fn p -> %>
+          <div class="player">
+          <span class={
+              case @self.role == :bad && p.role == :bad do
+                true -> "name bad"
+                false -> "name"
+              end}>
+              <%= p.name %>
+              <%= if p.is_king do %>
+                👑
+              <% end %>
+            </span>
+
+            <span class="vote">
+
+              <%= if @stage == :voting do %>
+                <%= case @team_votes[p.id] do %>
+                <% :approve -> %> 👍
+                <% :reject -> %> 👎
+                <% end %>
+              <% end %>
+
+              <%= if @stage == :quest && p.on_quest do %>
+              🏆
+              <% end %>
+            </span>
+
+            <%= if @stage == :party_assembling && @self.is_king do %>
+              <.button>
+                <%= case p.on_quest do
+                  true -> "Cancel"
+                  false -> "Select"
+                end %>
+              </.button>
+            <% end %>
+          </div>
+        <% end)%>
+      </div>
     """
   end
 end
--- lib/resistance_web/live/game/game_live.ex
+++ lib/resistance_web/live/game/game_live.ex
@@ -13,14 +13,17 @@ defmodule ResistanceWeb.GameLive do
         {:ok, init_state}
       true ->
         Game.Server.subscribe()
-        {:ok, init_state |> assign(:state, Game.Server.get_state)}
+        state = Game.Server.get_state
+        {:ok, init_state
+          |> assign(:state, state)
+          |> assign(:self, get_self(id, state.players))}
     end
   end
 
   @impl true
   def handle_params(_params, _url, %{assigns: %{self: self} } = socket) do
     cond do
-      GenServer.whereis(Game.Server) == nil || !Game.Server.is_player(self) ->
+      GenServer.whereis(Game.Server) == nil || !Game.Server.is_player(self.id) ->
         {:noreply, push_navigate(socket, to: "/")}
       true ->
         {:noreply, socket}
@@ -37,7 +40,9 @@ defmodule ResistanceWeb.GameLive do
   @impl true
   def handle_info({:update, state}, socket) do
     IO.puts("Stage: #{inspect state.stage}")
-    {:noreply, socket |> assign(:state, state)}
+    {:noreply, socket
+      |> assign(:state, state)
+      |> assign(:self, get_self(socket.assigns.self.id, state.players))}
   end
 
   @impl true
@@ -46,5 +51,9 @@ defmodule ResistanceWeb.GameLive do
       Game.Server.message(socket.assigns.self, msg)
     end
     {:noreply, socket |> assign(:form, to_form(%{"message" => ""}))}
+  end
+
+  defp get_self(id, players) do
+    Enum.find(players, fn p -> p.id == id end)
   end
 end
