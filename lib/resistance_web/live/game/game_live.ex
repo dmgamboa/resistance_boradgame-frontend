6e4d697bcfdb7bbd7545e708d74dc267e91bdfdd
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2023-04-10 17:50:21 -0700

    Redirect to home screen if user not in game

--- lib/resistance_web/live/game/game_live.ex
+++ lib/resistance_web/live/game/game_live.ex
@@ -3,12 +3,26 @@ defmodule ResistanceWeb.GameLive do
 
   @impl true
   def mount(_params, session, socket) do
-    Game.Server.subscribe()
+    id = session["_csrf_token"]
     init_state = socket
-      |> assign(:self, session["_csrf_token"])
-      |> assign(:form, to_form(%{"message" => ""}))
-      |> assign(:messages, [])
+    |> assign(:self, session["_csrf_token"])
+    |> assign(:form, to_form(%{"message" => ""}))
+    |> assign(:messages, [])
+    case Pregame.Server.is_player(id) do
+      false -> {:ok, init_state}
+      true ->
+        Game.Server.subscribe()
+        {:ok, init_state |> assign(:players, Game.Server.get_players())}
+    end
     {:ok, init_state}
+  end
+
+  @impl true
+  def handle_params(_params, _url, %{assigns: %{self: self} } = socket) do
+    case Pregame.Server.is_player(self) do
+      false -> {:noreply, push_navigate(socket, to: "/")}
+      true -> {:noreply, socket}
+    end
   end
 
   @impl true
